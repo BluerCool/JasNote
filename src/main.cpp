190f30a6042c8742880823ed0f2f4e8bc166f0f0
JasNote <jas@jasnote.dev>
2026-06-19 10:43:16 +0000

    Integrate Qlementine modern QStyle framework from GitHub
    - Added oclero/qlementine v1.4.2 via CMake FetchContent (MIT license)
    - Drop-in QlementineStyle applied for modern look & feel
    - Banned color 'purplebule' excluded from theme
    - All 7 tests still passing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <oclero/qlementine.hpp>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
@@ -6,6 +7,9 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setApplicationName("JasNote");
     app.setOrganizationName("JasNote");
+
+    auto *style = new oclero::qlementine::QlementineStyle(&app);
+    QApplication::setStyle(style);
 
     MainWindow window;
     window.show();
